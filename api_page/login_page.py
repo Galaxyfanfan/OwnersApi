10e84fa34a7e9aafc2e606cd3dcf3d62cdd820d1
Galaxyfanfan <852347376@qq.com>
2020-11-21 15:40:07 +0800

    初始化

---
+++ api_page/login_page.py
@@ -0,0 +1,22 @@
+"""
+OwnersApi - 当前Project名称;
+login_api - 在创建文件的对话框中指定的文件名;
+galaxy - 当前用户名;
+2020/11/21 3:30 下午 
+"""
+from api_page.base_api import BaseApi
+
+
+class LoginPage(BaseApi):
+    """
+    通讯录管理，包括增删改查
+    """
+
+    def __init__(self):
+
+
+    def get_code(self):
+        pass
+
+    def login(self):
+        pass
---
+++ api_page/wework_utils.py
@@ -0,0 +1,13 @@
+from api_page.base_api import BaseApi
+
+
+class WeWorkUtils(BaseApi):
+    def get_token(self, corpsecret, corpid="wwe653983e4c732493"):
+        data = {
+            "method": "get",
+            "url": f"https://qyapi.weixin.qq.com/cgi-bin/gettoken",
+            "params": {"corpid": corpid, "corpsecret": corpsecret}
+        }
+        print('get===token')
+        result = self.send_api(data)
+        return result["access_token"]
