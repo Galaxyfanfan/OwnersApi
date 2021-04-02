3be8c9717ad6b751fa47c0ac97bd6a8fca4212a7
Galaxyfanfan <852347376@qq.com>
2021-04-02 10:33:22 +0800

    w

---
+++ config/config.py
@@ -0,0 +1,51 @@
+"""
+OwnersApi - 当前Project名称;
+config - 在创建文件的对话框中指定的文件名;
+galaxy - 当前用户名;
+2020/11/21 5:37 下午 
+"""
+import yaml
+import os
+import threading
+class Config(object):
+    #----------------单例-------------------
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        pass
+
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(Config, "_instance"):
+            with Config._instance_lock:
+                if not hasattr(Config, "_instance"):
+                    Config._instance = object.__new__(cls)
+        return Config._instance
+
+    #-----------------------------------------
+
+    basedir = os.path.dirname(__file__)
+    path = basedir + '/userinfo.yaml'
+    userinfo = yaml.safe_load(open(path))
+
+    appkey = 'b91714e8654c75b298fda4029685101f'
+    baseurl = "http://daopu.chejinkuang.com/api/consumer/"
+    uuid = 'B1566FDC-2F4D-4FE2-B8D7-A0558E771536'
+
+    if not userinfo:
+        token = ''
+        uid = ''
+        username = ''
+        cellphone = ''
+        idno = ''
+        accid = ''
+    else:
+        token = userinfo['token']
+        uid = str(userinfo['userInfo']['id'])
+        username = userinfo['userInfo']['username']
+        cellphone = userinfo['userInfo']['cellphone']
+        idno = userinfo['userInfo']['identity_card_num']
+        accid = userinfo['userInfo']['yx_video_accid']
+
+
+    islogin = token != ''
